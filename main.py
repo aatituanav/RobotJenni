2267ba26f59914071b8c23c523b1b39759ef7667
Andres Filomeno <alvaro.andres1996@hotmail.com>
2022-12-15 00:16:50 -0500

    Send Messages To Client of Lucia

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 import operations.operations as operations
 from dotenv import load_dotenv
 import os 
+import time
 
 #cargo las variables de entorno
 load_dotenv()
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 projectName = os.getenv("PROJECTNAME")
+userdata =  os.getenv("USERDATA")
 
 #configuracion de opciones para evitar que el navegador se cierre una vez ejecutadas las instrucciones
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+options = webdriver.ChromeOptions() 
+options.add_argument('--user-data-dir=%s' % (userdata))
+options.add_experimental_option("detach", True)
+
 
 
 #instanciamiento del webdriver
-browser = webdriver.Chrome(chrome_options=chrome_options)
+browser = webdriver.Chrome(executable_path="chromedriver.exe", options=options)
+
 #abrir la paguina
 browser.get('https://admin.trivo.com.ec/login')
 
+#browser.get('https://web.whatsapp.com/send?l=es&phone=593994938897&text=Hola%20XXXX%2C%20%C2%A1Queremos%20saber%20de%20ti!%20Hace%20unos%20d%C3%ADas%20compartimos%20contigo%20las%20opciones%20de%20las%20unidades%20habitacionales%20del%20Conjunto%20Residencial%20Luc%C3%ADa%20que%20m%C3%A1s%20se%20ajustan%20a%20lo%20que%20buscas.%20Si%20tienes%20alguna%20duda%20o%20ya%20te%20decidiste%20por%20tus%20favoritas%2C%20ponte%20en%20contacto%20conmigo%20para%20agendar%20tu%20visita.')
 
-operations.iniciarSesionTrivo(browser, email, password, projectName)
+#operations.sendInfoWhatsApp(browser)
+##NO HACE FALTA INICIAR SESION SI INICIO CON EL PERFIL GUARDADO AL ABRIR EL NAVEGADOR
+#operations.iniciarSesionTrivo(browser, email, password, projectName)
 
 operations.orderListByDate(browser)
+
+operations.entertoCustomer(browser)
--- operations/operations.py
+++ operations/operations.py
@@ -25,7 +25,7 @@ def iniciarSesionTrivo(browser, email, password, projectName):
         #encuentro el campo de texto para contrasenia
         passwordField = browser.find_element(By.XPATH, '//*[@id="root"]/div/div/div/div/form/div/div[2]/div/input')
         passwordField.send_keys(password)
-        time.sleep(0.5)
+        time.sleep(1)
         passwordField.send_keys(Keys.ENTER)
     except Exception as e: 
         print(e)
@@ -36,13 +36,148 @@ def orderListByDate(browser):
     try:
         #espero a que se cargue el campo de texto del correo
         lastDayContactHeader = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/table/thead/tr/th[12]')) #This is a dummy element
+            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/table/thead/tr/th[12]')) 
         )
         lastDayContactHeader.click()
         lastDayContactHeader = WebDriverWait(browser, 10).until(
-            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/table/thead/tr/th[12]')) #This is a dummy element
+            EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div/div[2]/div/div/div[2]/table/thead/tr/th[12]'))
         )
         lastDayContactHeader.click()
     except Exception as e: 
         print(e)
         browser.quit()
+
+
+
+def sendInfoWhatsApp(browser, phone, messages):
+    phone = '+593993055278'
+    try:
+        #cambio a la nueva ventana que se abrio automaticamente
+        browser.switch_to.window(browser.window_handles[1])
+        ##verifico que sea un numero de celular con +593 para quitarle
+        if phone.find('+') != -1:
+            browser.get('https://web.whatsapp.com/send?l=es&phone=%s&text=' %(phone.lstrip('+'))) 
+
+            #espero a que la caja de texto del whatsapp se muestre para poder ingresar 
+            messageBoxText = WebDriverWait(browser, 40).until(
+                EC.presence_of_element_located((By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'))
+            )
+            #cuando se inicia, se cargan todos los elementos, pero aparece un popup de cargando mensajes...tengo que esperar a que se quite
+            WebDriverWait(browser, 30).until(
+                EC.invisibility_of_element_located((By.XPATH,'//*[@id="app"]/div/span[2]/div/span/div/div/div/div'))
+            )
+            for msg in messages:
+                messageBoxText.send_keys(msg)
+                messageBoxText.send_keys(Keys.ENTER)
+                time.sleep(2)
+            print('cambio a la ventana anterior')
+            browser.switch_to.window(browser.window_handles[0])
+
+        else:
+            print('SE ENCONTRO UN NUMERO DE TELEFONO INVALIDO, PROGRAMAR LA LOGICA PRRO')
+            browser.quit()
+        
+    except Exception as e: 
+        print(e)
+        browser.quit()
+
+def entertoCustomer(browser):
+    name = ''
+    phoneNumber = ''
+    messages = ['','Buenos días XXXX le saluda Katherine Quintana, Asesora Inmobiliaria de trivo.','Le escribo porque queremos saber si aún tiene interés en adquirir bienes inmueble.']
+    linkWhatsaPo = ''
+
+    def findCustomertoEdit(browser):
+        #Los nuevos usuarios aparecen con el campo "FECHA ULTIMO CONTACTO" vacio, por lo cual no hay que hacer seguimiento de dichos usuarios
+        #Por lo tanto al ordenar por fecha, estos usuarios aparecen primero, por lo cual es necesario saber en que fila estan los usuarios no nuevos 
+        #de esta manera si hay 4 usuarios nuevos, habria que hacer seguimiento al 5 usuario (siempre y cuando la tabla este ordenada por fecha)
+        #este metodo regresara el xpath del componente que el robot tendra que hacer click
+        numberofRows = 20
+        ##la informacion se muestra desde la posicion 1, (0 es para la cabecera)
+
+        WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div[2]/table/tbody/tr[1]/td[12]'))
+        )
+
+        for i in range(1, numberofRows + 1):
+            selectField = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div[2]/table/tbody/tr[%s]/td[12]' % (i))
+            if selectField.text != '-':
+                return ('//*[@id="root"]/div/div[2]/div/div/div[2]/table/tbody/tr[%s]/td[2]' % (i))
+
+        print('TODAS LOS CLIENTES QUE SE MUESTRAN EN LA TABLA SON NUEVOS')
+        return ''
+
+    
+
+    try:
+        #hago click en el cliente que mas tiempo le tengo olvidado
+        xpath = findCustomertoEdit(browser)
+        editButton = browser.find_element(By.XPATH, xpath)
+        editButton.click()
+        
+        #espero a que cargue el boton de "SIGUIENTES ACCIONES" para pasar al siguiente cuestionario
+        siguientesAccionesButton = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[1]/div[2]/div/table/tbody/tr/td[2]/div/button'))
+        )
+        #obtengo el nombre del cliente
+        nameField = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[1]/div[2]/div/table/tbody/tr/td[1]/div/div[1]/h4')
+        name = nameField.text
+
+        
+        #obtengo el numero de celular del cliente
+        phoneField = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[1]/div[2]/div/table/tbody/tr/td[1]/div/div[2]/a/h6')
+        phoneNumber = phoneField.text.strip()
+
+        #avanzo a la siguiente paguina
+        siguientesAccionesButton.click()
+        
+        #espero que termine de cargar la paguina (que desaparezca el loadbutton)
+        WebDriverWait(browser, 10).until(
+            EC.invisibility_of_element_located((By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[1]/div/*[name()="svg"]'))
+        )
+
+        ##lleno los datos del cuestinoario y continuo con la siguiente paguina
+        #muestro las opciones
+        selectField = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div/div[2]/div[3]/div[2]/div/div/div/div')
+        selectField.click()
+
+       #selecciono la opcion de seguimiento
+        selectFieldSecondOption = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@id="menu-Enviar whatsapp"]/div[2]/ul/li[3]'))
+        )
+        selectFieldSecondOption.click()
+        
+        #copio el mensaje que se enviara al whatsapo
+        messageField = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div/div[2]/div[3]/div[2]/div[2]/div[2]/div'))
+        )  
+        #guardo el mensaje
+        messages[0] = messageField.text.replace('XXXX', name)
+        #en el segundo mensaje del array, el nombre esta con XXXX
+        messages[1] = messages[1].replace('XXXX', name.split()[0])
+
+
+        #click en el boton para continuar (Esto abre whatsapp en una nueva ventana)
+        continueButton = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div/div[2]/div[10]/div/button')
+        continueButton.click()
+        
+        #confirmar que deseo continuar
+        confirmButton = WebDriverWait(browser, 10).until(
+            EC.element_to_be_clickable((By.XPATH,'/html/body/div[4]/div[2]/div/div[3]/button[2]'))
+        )  
+        confirmButton.click()
+
+        #envio info por whatsapp
+        sendInfoWhatsApp(browser, phoneNumber, messages)
+
+        #regreso a donde inicie (A la tabla de clientes)
+        goBackFirst = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div/div[1]/div/button')
+        goBackFirst.click()
+        goBackTable = browser.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div/div/div/div[1]/div[1]/button')
+        goBackTable.click()
+
+    except Exception as e: 
+        print(e)
+        browser.quit()
+
+    
